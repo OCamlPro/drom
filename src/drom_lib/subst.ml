f2c5d2475db965804a82dba643666515ff0dd54c
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2020-11-25 22:19:23 +0100

    update version and fix authors-as-strings in toml problem

--- src/drom_lib/subst.ml
+++ src/drom_lib/subst.ml
@@ -40,6 +40,8 @@ let project_brace (_, p) v =
   | "github-organization" -> maybe_string p.github_organization
   | "authors-as-strings" ->
       String.concat ", " (List.map (Printf.sprintf "%S") p.authors)
+  | "authors-for-toml" ->
+      String.concat ", " (List.map (Printf.sprintf "\"%s\"") p.authors)
   | "copyright" -> (
       match p.copyright with
       | Some copyright -> copyright
