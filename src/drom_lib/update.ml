517ec7cebf1ba85120d9416ed3be2d171b0485df
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-01-25 13:19:22 +0100

    set version 0.3.0

--- src/drom_lib/update.ml
+++ src/drom_lib/update.ml
@@ -201,40 +201,6 @@ let update_files ?args ?(git = false) ?(create = false) p =
     else
       (p, changed)
   in
-  (*
-  let p, changed =
-    match mode with
-    | None -> (p, changed)
-    | Some mode ->
-        let js_dep = ("js_of_ocaml", [ Semantic (3, 6, 0) ]) in
-        let js_tool = ("js_of_ocaml", [ Semantic (3, 6, 0) ]) in
-        let ppx_tool = ("js_of_ocaml-ppx", [ Semantic (3, 6, 0) ]) in
-        let add_dep (name, depversions) deps changed =
-          let dep =
-            ( name,
-              { depversions; depname = None; deptest = false; depdoc = false } )
-          in
-          match mode with
-          | Binary ->
-              if List.mem dep deps then
-                (EzList.remove dep deps, true)
-              else
-                (deps, changed)
-          | Javascript ->
-              if not (List.mem_assoc (fst dep) deps) then
-                (dep :: deps, true)
-              else
-                (deps, changed)
-        in
-        let dependencies, changed = add_dep js_dep p.dependencies changed in
-        let tools, changed = add_dep js_tool p.tools changed in
-        let tools, changed = add_dep ppx_tool tools changed in
-        List.iter (fun package ->
-            package.p_mode <- Some mode
-          ) p.packages;
-        ({ p with dependencies; tools }, changed)
-  in
-*)
   List.iter (fun package -> package.project <- p) p.packages;
 
   Hashes.with_ctxt ~git (fun hashes ->
