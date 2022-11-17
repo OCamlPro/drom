1d3bd85746867ffd051bfffc52e9d2de1d1f2d98
zapashcanon <leo@ndrs.fr>
2022-11-17 16:53:49 +0000

    Deploying to gh-pages from  @ 18252516478744d419706f718b08a388cdac7461 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (5fb724b) (2022/10/08 10:35)',
+    VERSION: 'master (1825251) (2022/11/17 16:53)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
