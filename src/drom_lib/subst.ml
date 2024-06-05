9fd69968035240e2604d364755c755790ba82f70
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2024-06-05 13:26:46 +0200

    Check drom version before modifying behavior of dune fields

--- src/drom_lib/subst.ml
+++ src/drom_lib/subst.ml
@@ -268,14 +268,16 @@ let project_brace ({ p; _ }  as state ) v =
       end )
   (* for dune *)
   | "dune-version" -> p.dune_version
-  | "dune-lang" -> 
-    (* just parsing basic semver for now. *)
-    begin
-      try 
-        Scanf.sscanf p.dune_version "%i.%i" 
-          (fun major minor -> Printf.sprintf "%i.%i" major minor)
-      with _ -> raise (Failure ("Cannot parse dune-version: " ^ p.dune_version))
-    end
+  | "dune-lang" ->
+      if VersionCompare.compare state.share.drom_version "0.9.2" >= 0 then
+        (* just parsing basic semver for now. *)
+        try
+          Scanf.sscanf p.dune_version "%i.%i"
+            (fun major minor -> Printf.sprintf "%i.%i" major minor)
+        with _ ->
+          raise (Failure ("Cannot parse dune-version: " ^ p.dune_version))
+      else
+        String.sub p.dune_version 0 (String.rindex p.dune_version '.')
   | "dune-cram" ->
     if VersionCompare.compare p.dune_version "2.7.0" >= 0 then
       "(cram enable)"
