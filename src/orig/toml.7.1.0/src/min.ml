8f499c80384f9e15801d95fd5434541634a8cbbb
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2025-10-27 21:42:03 +0100

    remove useless files

--- src/orig/toml.7.1.0/src/min.ml
+++
@@ -1,3 +0,0 @@
-let key = Types.Table.Key.of_string
-
-let of_key_values = Types.Table.of_key_values
--- src/orig/toml.7.1.0/tests/types_test.ml
+++
@@ -1,3 +0,0 @@
-let () =
-  let key = Toml.Types.Table.Key.of_string "a-b-c" in
-  assert (String.compare "a-b-c" (Toml.Types.Table.Key.to_string key) = 0)
